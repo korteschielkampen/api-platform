50fb295f43901aaa38580f07457bd02ff95eeda2
Zaturrby <rjkorteschiel@gmail.com>
2018-04-25 19:34:07 +0200

    wrap fucntion

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,17 @@ import styles from './index.module.css'
 const lambdaURL =
   process.env === 'production' ? '/.netlify/functions' : '/localhost:9000'
 
-fetch(`${lambdaURL}/hello`)
+const getData = async () => {
+  fetch(`${lambdaURL}/hello`)
   .then(res => res.json())
   .then(data => console.log(data.hello))
   .catch(err => console.error(err))
+}
 
 const IndexPage = () => (
   <div className={styles.container}>
     <div className={styles.content}>
-      <button> Click me </button>
+      <button onClick={getData}> Click me </button>
     </div>
   </div>
 )

5d091a7eb88a89f0ee3cc70d4c10aac3a17a105d
Zaturrby <rjkorteschiel@gmail.com>
2018-04-25 19:49:57 +0200

    visually working

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,31 @@ import styles from './index.module.css'
 const lambdaURL =
   process.env === 'production' ? '/.netlify/functions' : '/localhost:9000'
 
-const getData = async () => {
-  fetch(`${lambdaURL}/hello`)
-  .then(res => res.json())
-  .then(data => console.log(data.hello))
-  .catch(err => console.error(err))
+class IndexPage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {response: "No response yet...."}
+  }
+
+  getData = async () => {
+    fetch(`${lambdaURL}/hello`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(this)
+      this.setState({response: data.hello})
+    })
+    .catch(err => console.error(err))
+  }
+
+  render () {
+    return (
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <button onClick={this.getData} className={styles.button}> Click me </button>
+          <p>{this.state.response}</p>
+        </div>
+      </div>
+    )
+  }
 }
-
-const IndexPage = () => (
-  <div className={styles.container}>
-    <div className={styles.content}>
-      <button onClick={getData}> Click me </button>
-    </div>
-  </div>
-)
-
 export default IndexPage

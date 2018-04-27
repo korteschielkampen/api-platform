7d8cdbac88543790747e1fd8750897fa1fe8905c
Zaturrby <rjkorteschiel@gmail.com>
2018-04-27 19:25:32 +0200

    hide client

--- src/lambda/auth/lightspeed/refresh-tokens.js
+++ src/lambda/auth/lightspeed/refresh-tokens.js
@@ -2,8 +2,8 @@ const fetch = require('node-fetch');
 
 export default refreshTokens = async (code, callback) => {
   const payload = {
-    client_id: "4c23f9e681c44d339359a38dc340522fae805ddab5e372c39762ef91c080179d",
-    client_secret: process.env.LIGHTSPEED,
+    client_id: process.env.LIGHTSPEED_CLIENT,
+    client_secret: process.env.LIGHTSPEED_SECRET,
     refresh_token: code,
     grant_type: "refresh_token",
   };
--- src/pages/lightspeed-start.js
+++ src/pages/lightspeed-start.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 import styles from './index.module.css'
 
-const client_id = "4c23f9e681c44d339359a38dc340522fae805ddab5e372c39762ef91c080179d"
+const client_id = process.env.LIGHTSPEED_CLIENT
 const scope = "employee:reports"
 const temporaryTokenLink = `https://cloud.lightspeedapp.com/oauth/authorize.php?response_type=code&client_id=${client_id}&scope=${scope}`
 

2e3c176d5f1309a17f1eaa49bab54adf842f9759
Zaturrby <rjkorteschiel@gmail.com>
2018-04-28 17:36:36 +0200

    auth possibly wokring

--- functions/moneybird-auth-init.js
+++ functions/moneybird-auth-init.js
@@ -33934,24 +33934,14 @@ exports.handler = (() => {
     };
 
     try {
-      console.log("----------------INVOCATION HMM HMMM----------------");
       let tokens = yield (0, _createTokens2.default)(event.queryStringParameters.code);
-      console.log("----------------TOKENS HMM HMMM----------------");
-      console.log(tokens);
-      console.log("----------------TOKENS HMM HMMM----------------");
       let account = yield (0, _readAdministration2.default)(tokens.access_token);
-      console.log("----------------ACCOUNT HMM HMMM----------------");
-      console.log(account);
-      console.log("----------------ACCOUNT HMM HMMM----------------");
       let authData = {
-        'account_id': parseInt(account.account[0].id),
-        'account_name': account.account[0].name,
+        'account_id': parseInt(account[0].id),
+        'account_name': account[0].name,
         'access_token': tokens.access_token,
         'refresh_token': tokens.refresh_token
       };
-      console.log("----------------AUTHDATA HMM HMMM----------------");
-      console.log(authData);
-      console.log("----------------AUTHDATA HMM HMMM----------------");
       let dynamo = yield (0, _update2.default)(authData);
 
       respond({ status: 200, body: { authData: authData, stored: dynamo } });
--- src/lambda/moneybird-auth-init.js
+++ src/lambda/moneybird-auth-init.js
@@ -12,24 +12,14 @@ exports.handler = async (event, context, callback) => {
   };
 
   try {
-    console.log("----------------INVOCATION HMM HMMM----------------")
     let tokens = await createTokens(event.queryStringParameters.code);
-    console.log("----------------TOKENS HMM HMMM----------------")
-    console.log(tokens)
-    console.log("----------------TOKENS HMM HMMM----------------")
     let account = await readAccount(tokens.access_token);
-    console.log("----------------ACCOUNT HMM HMMM----------------")
-    console.log(account)
-    console.log("----------------ACCOUNT HMM HMMM----------------")
     let authData = {
-      'account_id' : parseInt(account.account[0].id),
-      'account_name' : account.account[0].name,
+      'account_id' : parseInt(account[0].id),
+      'account_name' : account[0].name,
       'access_token' : tokens.access_token,
       'refresh_token' : tokens.refresh_token
     }
-    console.log("----------------AUTHDATA HMM HMMM----------------")
-    console.log(authData)
-    console.log("----------------AUTHDATA HMM HMMM----------------")
     let dynamo = await updateDynamo(authData);
 
     respond({ status: 200, body: {authData: authData, stored: dynamo}});

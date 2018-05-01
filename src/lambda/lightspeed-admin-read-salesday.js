d179c7a87c688c55388c4190e434e4e5bdef6f22
Zaturrby <rjkorteschiel@gmail.com>
2018-05-01 17:25:07 +0200

    conditionally request from Lambda

--- src/lambda/lightspeed-admin-read-salesday.js
+++ src/lambda/lightspeed-admin-read-salesday.js
@@ -2,6 +2,7 @@ import _ from 'lodash'
 
 import readSalesDay from './api/lightspeed/read-sales-day.js'
 import updateDynamo from './general/dynamo/update.js'
+import readDynamo from './general/dynamo/read.js'
 
 exports.handler = async (event, context, callback) => {
   const respond = ({ status, body }) => {
@@ -13,33 +14,50 @@ exports.handler = async (event, context, callback) => {
 
   try {
     let date = JSON.parse(event.body).date;
-    let sales = await readSalesDay(date);
+    let lsRequested = false;
 
-    _.each(sales, (sale)=>{
-      _.each(sale, (value, key)=>{
-        console.log(key, ": ", value)
-        if (value === "" || value === null) {
-          delete sale[key];
-        }
-      })
-    })
-
+    // Download day from Dynamo
     var params = {
       TableName: 'lightspeed-sales-day',
-      Item: {
-        timeStamp: date,
-        sales: sales
+      Key: {
+        "timeStamp": date,
       }
     };
-    let dbStatus = updateDynamo(params);
+    let salesDay = await readDynamo(params);
+
+    //  When not in Dynamo download from Lightspeed and put in Dynamo
+    let lsRequest = !salesDay;
+    if (lsRequest) {
+      lsRequested = true;
+      let sales = await readSalesDay(date);
+
+      _.each(sales, (sale)=>{
+        _.each(sale, (value, key)=>{
+          console.log(key, ": ", value)
+          if (value === "" || value === null) {
+            delete sale[key];
+          }
+        })
+      })
+
+      salesDay = {
+        TableName: 'lightspeed-sales-day',
+        Item: {
+          timeStamp: date,
+          sales: sales
+        }
+      };
+      await updateDynamo(salesDay);
+      salesDay = salesDay.Item
+    }
 
     respond({
       status: 200,
       body: {
         sales: {
-          ...sales
+          ...salesDay
         },
-        dbStatus: dbStatus
+        lightspeed: lsRequested
       }
     });
 

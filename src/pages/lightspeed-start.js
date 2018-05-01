c55f8db0291c090538ed8e33b934ea0aa6e73db3
Zaturrby <rjkorteschiel@gmail.com>
2018-05-01 13:04:45 +0200

    fix lightspeed bug

--- src/pages/lightspeed-start.js
+++ src/pages/lightspeed-start.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 import styles from './index.module.css'
 
 const client_id = "4c23f9e681c44d339359a38dc340522fae805ddab5e372c39762ef91c080179d";
-const scope = "employee:reports+lemployee:register_read"
+const scope = "employee:reports+employee:register_read"
 const temporaryTokenLink = `https://cloud.lightspeedapp.com/oauth/authorize.php?response_type=code&client_id=${client_id}&scope=${scope}`
 
 class IndexPage extends React.Component {

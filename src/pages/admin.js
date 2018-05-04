2a88ff6cf75c223f02eafeb1c6e8b6bc93e9443d
Zaturrby <rjkorteschiel@gmail.com>
2018-05-04 20:55:41 +0200

    some simple and reasonably insecure auth

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -4,6 +4,7 @@ import _ from 'underscore'
 import classNames from 'classnames'
 import moment from 'moment'
 import DatePicker from 'react-datepicker'
+import netlifyIdentity from 'netlify-identity-widget'
 
 import styles from './index.module.css'
 import 'react-datepicker/dist/react-datepicker-cssmodules.css'
@@ -21,6 +22,7 @@ class IndexPage extends React.Component {
       dates: { start: moment(), end: moment() },
       status: 'Nog geen data aangevraagd',
       statusColor: 'grey',
+      login: false,
     }
     this.handleStartDateChange = this.handleStartDateChange.bind(this)
     this.handleEndDateChange = this.handleEndDateChange.bind(this)
@@ -51,6 +53,20 @@ class IndexPage extends React.Component {
         },
       }
     })
+  }
+
+  componentDidMount() {
+    netlifyIdentity.init()
+    netlifyIdentity.on('login', user => this.setState({ login: true }))
+    netlifyIdentity.on('logout', user => this.setState({ login: false }))
+  }
+
+  login() {
+    netlifyIdentity.open()
+  }
+
+  logout() {
+    netlifyIdentity.logout()
   }
 
   async getReports(dayreport) {
@@ -171,6 +187,11 @@ class IndexPage extends React.Component {
       <div className={styles.container}>
         <div className={styles.content}>
           <p>Admin voor Lightspeed API naar Moneybird API integratie</p>
+          <button className={classNames(styles.button, styles.buttonGrey)} onClick={this.login.bind(this)}> Login </button>
+          <button className={classNames(styles.button, styles.buttonGrey)} onClick={this.logout.bind(this)}> logout </button>
+        </div>
+
+        <div className={styles.content}>
           <h1>Status</h1>
           <p
             style={{ backgroundColor: this.state.statusColor }}
@@ -179,87 +200,88 @@ class IndexPage extends React.Component {
             {this.state.status}
           </p>
         </div>
-        <div className={styles.content}>
-          <h1>Reports</h1>
-          <div className={styles.box}>
-            <div className={styles.datepickerWrapper}>
-              <DatePicker
-                className={styles.datepicker}
-                selected={this.state.dates.start}
-                startDate={this.state.dates.start}
-                endDate={this.state.dates.end}
-                onChange={this.handleStartDateChange}
-                selectsStart
-              />
-              <DatePicker
-                className={styles.datepicker}
-                selected={this.state.dates.end}
-                startDate={this.state.dates.start}
-                endDate={this.state.dates.end}
-                onChange={this.handleEndDateChange}
-                selectsEnd
-              />
-            </div>
-            <button className={styles.button} onClick={this.getReports}>
-              Verkrijg rapporten
-            </button>
-            <button className={styles.button} onClick={this.testAutomate}>
-              Test automate
-            </button>
-          </div>
-          {this.state.dayreports.map((dayreport, key) => {
-            return (
-              <div key={key} className={styles.card}>
-                <div className={styles.cardHeader}>
-                  <p className={styles.cardHeading}>
-                    Start: {moment(dayreport.date).format('MM/DD/YYYY')}
-                  </p>
-                  <p className={styles.cardHeadingText}>
-                    {dayreport.lsRequested ? 'Lightspeed' : 'DynamoDB'}
-                  </p>
-                  <button
-                    className={classNames(styles.button)}
-                    onClick={this.getReports.bind(this, dayreport)}
-                  >
-                    Refresh
-                  </button>
-                  <button
-                    className={classNames(styles.button, styles.buttonBlue)}
-                    onClick={this.createInvoice.bind(this, dayreport)}
-                  >
-                    Sla op in Moneybird
-                  </button>
-                </div>
-                <div className={styles.cardBody}>
-                  <div className={styles.cardItem}>
-                    {Object.keys(dayreport.tax).map(key => {
-                      return (
-                        <p key={key} className={styles.cardText}>
-                          <span style={{ color: 'grey' }}>
-                            Belastingtype -
-                          </span>
-                          {key}: {dayreport.tax[key].amount}
-                        </p>
-                      )
-                    })}
-                  </div>
-                  <div className={styles.cardItem}>
-                    {Object.keys(dayreport.payments).map(key => {
-                      return (
-                        <p key={key} className={styles.cardText}>
-                          <span style={{ color: 'grey' }}>
-                            Betalingen -
-                          </span>
-                          {key}: {dayreport.payments[key].amount}
-                        </p>
-                      )
-                    })}
-                  </div>
-                </div>
+
+        {this.state.login && (
+          <div className={styles.content}>
+            <h1>Reports</h1>
+            <div className={styles.box}>
+              <div className={styles.datepickerWrapper}>
+                <DatePicker
+                  className={styles.datepicker}
+                  selected={this.state.dates.start}
+                  startDate={this.state.dates.start}
+                  endDate={this.state.dates.end}
+                  onChange={this.handleStartDateChange}
+                  selectsStart
+                />
+                <DatePicker
+                  className={styles.datepicker}
+                  selected={this.state.dates.end}
+                  startDate={this.state.dates.start}
+                  endDate={this.state.dates.end}
+                  onChange={this.handleEndDateChange}
+                  selectsEnd
+                />
               </div>
-            )
-          })}
-        </div>
+              <button className={styles.button} onClick={this.getReports}>
+                Verkrijg rapporten
+              </button>
+              <button className={styles.button} onClick={this.testAutomate}>
+                Test automate
+              </button>
+            </div>
+            {this.state.dayreports.map((dayreport, key) => {
+              return (
+                <div key={key} className={styles.card}>
+                  <div className={styles.cardHeader}>
+                    <p className={styles.cardHeading}>
+                      Start: {moment(dayreport.date).format('MM/DD/YYYY')}
+                    </p>
+                    <p className={styles.cardHeadingText}>
+                      {dayreport.lsRequested ? 'Lightspeed' : 'DynamoDB'}
+                    </p>
+                    <button
+                      className={classNames(styles.button)}
+                      onClick={this.getReports.bind(this, dayreport)}
+                    >
+                      Refresh
+                    </button>
+                    <button
+                      className={classNames(styles.button, styles.buttonBlue)}
+                      onClick={this.createInvoice.bind(this, dayreport)}
+                    >
+                      Sla op in Moneybird
+                    </button>
+                  </div>
+                  <div className={styles.cardBody}>
+                    <div className={styles.cardItem}>
+                      {Object.keys(dayreport.tax).map(key => {
+                        return (
+                          <p key={key} className={styles.cardText}>
+                            <span style={{ color: 'grey' }}>
+                              Belastingtype -
+                            </span>
+                            {key}: {dayreport.tax[key].amount}
+                          </p>
+                        )
+                      })}
+                    </div>
+                    <div className={styles.cardItem}>
+                      {Object.keys(dayreport.payments).map(key => {
+                        return (
+                          <p key={key} className={styles.cardText}>
+                            <span style={{ color: 'grey' }}>Betalingen -</span>
+                            {key}: {dayreport.payments[key].amount}
+                          </p>
+                        )
+                      })}
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        )}
       </div>
     )
   }

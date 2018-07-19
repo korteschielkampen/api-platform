a3b55d757ee5ddf6e3c5163c5a1f1cacd61aecc9
Zaturrby <rjkorteschiel@gmail.com>
2018-07-19 09:44:21 +0200

    update integration plugin to fix last nights error

--- src/lambda/integration-accountancy.js
+++ src/lambda/integration-accountancy.js
@@ -13,23 +13,24 @@ exports.handler = async (event, context, callback) => {
 
   try {
     // Setup date
-    let dates = event.queryStringParameters.date
-      ? {
-          start: moment(event.queryStringParameters.date)
-            .startOf('day')
-            .format(),
-          end: moment(event.queryStringParameters.date)
-            .endOf('day')
-            .format(),
-        }
-      : {
-          start: moment()
-            .startOf('day')
-            .format(),
-          end: moment()
-            .endOf('day')
-            .format(),
-        }
+    let dates =
+      event.queryStringParameters && event.queryStringParameters.date
+        ? {
+            start: moment(event.queryStringParameters.date)
+              .startOf('day')
+              .format(),
+            end: moment(event.queryStringParameters.date)
+              .endOf('day')
+              .format(),
+          }
+        : {
+            start: moment()
+              .startOf('day')
+              .format(),
+            end: moment()
+              .endOf('day')
+              .format(),
+          }
 
     let postSlack = {
       post: false,
@@ -38,8 +39,9 @@ exports.handler = async (event, context, callback) => {
 
     // Generate business reports by day
     let dayReports = await createBusinessReport(dates, postSlack)
+
     // Create invoice at Moneybird
-    // await moneybirdCreate(dayReports[0])
+    await moneybirdCreate(dayReports[0])
 
     console.log('everything is done')
     respond({ status: 200, body: { message: 'succes' } })
---
+++ src/lambda/models/item/reorderpoint.js
@@ -0,0 +1,7 @@
+export default (sales, items) => {
+  /*
+  What to do here. Might be best to throw in an item and return a verdict
+  This is the preferred lvl, this is the reorder point.
+  */
+  return items
+}

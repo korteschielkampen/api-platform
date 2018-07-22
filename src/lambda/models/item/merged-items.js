6e464a718915f270222622e1ff6fb578c0d8097b
Zaturrby <rjkorteschiel@gmail.com>
2018-07-22 16:59:51 +0200

    decided

--- src/lambda/models/item/merged-items.js
+++ src/lambda/models/item/merged-items.js
@@ -1,17 +1,4 @@
-import _ from 'lodash'
-
 export default (saleStatsByItem, items, options) => {
-  // Ive made it far worse that the previous JS implementation.
-  // Fastest: Just write the forEach and merge the maps manually.
-  // Most readable: Perform an reduce
-  let lodash = _.map(
-    _.merge(
-      _.keyBy(_.intersection(items, saleStatsByItem, 'itemID'), 'itemID'),
-      saleStatsByItem
-    ),
-    i => i
-  )
-
   let mergedItems = items.reduce((acc, i) => {
     saleStatsByItem[i.itemID] &&
       acc.push({

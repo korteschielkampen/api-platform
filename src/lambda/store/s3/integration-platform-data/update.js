fc2a9267f24bfaaaeb43b0e13eaa9fd21fe1f29c
Zaturrby <rjkorteschiel@gmail.com>
2018-08-05 21:09:44 +0200

    cread once a day, need a little bit more for once every few minutes

--- src/lambda/store/s3/integration-platform-data/update.js
+++ src/lambda/store/s3/integration-platform-data/update.js
@@ -21,13 +21,6 @@ export default async (datatype, data) => {
     Key: `${datatype}.json`,
   }
 
-  // // Put it in bucket
-  // s3.putObject(params, function(err, data) {
-  //   if (err)
-  //     console.log(err, err.stack) // an error occurred
-  //   else console.log(`---> ${datatype} S3 done`) // successful response
-  // })
-
   // Put it in bucket
   try {
     data = await s3.putObject(params).promise()

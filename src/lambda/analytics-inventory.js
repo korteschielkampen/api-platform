dc0063d1f4f0643426eec6751cefa1f4d1962dae
Zaturrby <rjkorteschiel@gmail.com>
2018-08-07 10:03:32 +0200

    sunburst update to s3 datastore

--- src/lambda/analytics-inventory.js
+++ src/lambda/analytics-inventory.js
@@ -2,6 +2,7 @@ import moment from 'moment'
 import fs from 'fs'
 
 import createAnalyticsInventory from './actions/create-analytics-inventory.js'
+import readData from './actions/read-data.js'
 
 exports.handler = async (event, context, callback) => {
   const respond = ({ status, body }) => {
@@ -15,11 +16,14 @@ exports.handler = async (event, context, callback) => {
     var callback = () => {}
   }
   try {
-    let sales = JSON.parse(fs.readFileSync('./static/data/sales.json'))
-    let items = JSON.parse(fs.readFileSync('./static/data/items.json'))
-    let categories = JSON.parse(
-      fs.readFileSync('./static/data/categories.json')
-    )
+    // let sales = JSON.parse(fs.readFileSync('./static/data/sales.json'))
+    // let items = JSON.parse(fs.readFileSync('./static/data/items.json'))
+    // let categories = JSON.parse(
+    //   fs.readFileSync('./static/data/categories.json')
+    // )
+
+    console.time('Getting data from AWS')
+    let { sales, items, categories } = await readData('all')
 
     let starburstData = createAnalyticsInventory(sales, items, categories)
 
